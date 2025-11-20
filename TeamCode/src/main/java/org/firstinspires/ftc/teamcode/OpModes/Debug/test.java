b9f497d14d282652aa595afb275e8e4081748757
Ardey0 <albuandreicezar@gmail.com>
2025-11-20 19:02:10 +0200

    add encoder debug teleop (sunteti vai de steaua voastra)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Debug/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Debug/test.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.OpModes.Debug;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.lynx.LynxModule;
