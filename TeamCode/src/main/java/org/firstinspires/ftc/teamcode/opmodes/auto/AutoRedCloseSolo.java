5b81ba6523a0d93e05cb627a6fbabb72de4ada23
teo <teodorterecoasa@gmail.com>
2026-03-11 16:08:13 +0200

    auto red cu luat de la gate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoRedCloseSolo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoRedCloseSolo.java
@@ -14,15 +14,21 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.seattlesolvers.solverslib.command.CommandScheduler;
+import com.seattlesolvers.solverslib.command.ConditionalCommand;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
 import com.seattlesolvers.solverslib.command.WaitCommand;
 import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
+import com.seattlesolvers.solverslib.pedroCommand.TurnToCommand;
 
 import org.firstinspires.ftc.teamcode.commands.Init;
+import org.firstinspires.ftc.teamcode.commands.IntakeIndexing;
 import org.firstinspires.ftc.teamcode.commands.Intake;
 import org.firstinspires.ftc.teamcode.commands.LaunchAll;
+import org.firstinspires.ftc.teamcode.commands.LaunchMotif;
+import org.firstinspires.ftc.teamcode.commands.ReadMotif;
+import org.firstinspires.ftc.teamcode.commands.SpitBalls;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.subsystems.IntakeKickerSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
@@ -39,7 +45,7 @@ import org.firstinspires.ftc.teamcode.subsystems.TiltSubsystem;
 
 @Autonomous
 public class AutoRedCloseSolo extends CommandOpMode {
-    private final int ALLIANCE = 1; // BLUE
+    private final int ALLIANCE = 1; // RED
 
     private TelemetryManager telemetryM;
 
@@ -47,9 +53,9 @@ public class AutoRedCloseSolo extends CommandOpMode {
     private PaleteSubsytem palete;
     private OnofreiSubsystem onofrei;
     private IntakeSubsystem intake;
-    private TiltSubsystem tilt;
     private IntakeKickerSubsystem intakeKicker;
     private RampaSubsystem rampa;
+    private TiltSubsystem tilt;
     private SenzorTavanSubsystem senzorTavan;
     private SenzorRoataSubsystem senzorRoata;
     private SenzorGauraSubsystem senzorGaura;
@@ -73,21 +79,23 @@ public class AutoRedCloseSolo extends CommandOpMode {
     public PathChain Launch3;
     public PathChain ClearGate;
     public PathChain Motif;
-    public PathChain LaunchGate;
     public PathChain GrabGate;
-    private final Pose start = new Pose(123.6, 121.1, Math.toRadians(129.2));
-    private final Pose launchPre = new Pose(88, 87.3, Math.toRadians(135));
+    public PathChain LaunchGate;
+
+    private final Pose start = new Pose(123, 120, Math.toRadians(129.3));
+    private final Pose launchPre = new Pose(97, 95, Math.toRadians(135));
     private final Pose motif = new Pose(45, 95, Math.toRadians(-180));
-    private final Pose launch1 = new Pose(53, 90, Math.toRadians(-129));
-    private final Pose launchEdge = new Pose(89, 80, Math.toRadians(139));
-    private final Pose launchFinal = new Pose(86, 99, Math.toRadians(128));
-    private final Pose grab1 = new Pose(120, 84.5, Math.toRadians(0));
-    private final Pose clearGateSafe = new Pose(13, 73, Math.toRadians(90));
-    private final Pose clearGateRisky = new Pose(13.7,64.5, Math.toRadians(180));
-    private final Pose grab2 = new Pose(126, 59, Math.toRadians(0));
-    private final Pose grab3 = new Pose(17, 35, Math.toRadians(180));
-    private final Pose grabGate = new Pose(129, 59, Math.toRadians(31.5));
-    private final Pose exit = new Pose(34, 75, Math.toRadians(-135));
+    private final Pose launch = new Pose(90, 90, Math.toRadians(134));
+    private final Pose launchFinal = new Pose(87, 102, Math.toRadians(131));
+    private final Pose launch3 = new Pose(86, 100, Math.toRadians(131));
+    private final Pose grab1 = new Pose(121, 81, Math.toRadians(0));
+    private final Pose clearGate = new Pose(128, 73 , Math.toRadians(90));
+    private final Pose grab2 = new Pose(126, 55, Math.toRadians(0));
+    private final Pose grab3 = new Pose(130, 31, Math.toRadians(0));
+    private final Pose exit = new Pose(150, 90, Math.toRadians(-135));
+    private final Pose grabGate = new Pose(132, 57, Math.toRadians(34.5));
+
+
 
     public void buildPaths() {
         preload = follower.pathBuilder()
@@ -105,24 +113,7 @@ public class AutoRedCloseSolo extends CommandOpMode {
 
         Grab1 = follower.pathBuilder()
                 .addPath(
-                        new BezierCurve(
-                                launchEdge,
-                                new Pose(72.4, 72.4),
-                                grab1)
-                )
-                .setLinearHeadingInterpolation(launchEdge.getHeading(),grab1.getHeading())
-                .build();
-        ClearGate = follower.pathBuilder()
-                .addPath(
-                        new BezierCurve(grab2,
-                                new Pose(21.66, 61.83),
-                                clearGateRisky)
-                )
-                .setConstantHeadingInterpolation(clearGateRisky.getHeading())
-                .build();
-        Launch1 = follower.pathBuilder()
-                .addPath(
-                        new BezierLine(grab1, launchFinal)
+                        new BezierLine(launch, grab1)
                 )
                 .addParametricCallback(0.6, () ->
                         follower.setMaxPower(0.5)
@@ -130,60 +121,48 @@ public class AutoRedCloseSolo extends CommandOpMode {
                 .addParametricCallback(1, () ->
                         follower.setMaxPower(1)
                 )
-                .setLinearHeadingInterpolation(grab1.getHeading(), launchFinal.getHeading())
+                .setConstantHeadingInterpolation(grab1.getHeading())
                 .build();
-        Grab2 = follower.pathBuilder()
-                .addPath(
-                        new BezierCurve(
-                                launchPre,
-                                new Pose(77.5, 44.2),
-                                grab2
-                        )
-                )
-                .addParametricCallback(0.75, () ->
-                        follower.setMaxPower(0.5)
-                )
-                .addParametricCallback(0.99, () ->
-                        follower.setMaxPower(1)
-                )
-                .setLinearHeadingInterpolation(launchPre.getHeading(), grab2.getHeading())
-                .build();
-        Launch2 = follower.pathBuilder()
-                .addPath(
-                        new BezierLine(
-                                grab2,
-                                launchEdge)
-                )
-                .addParametricCallback(0.6, () ->
-                        follower.setMaxPower(0.5)
-                )
-                .addParametricCallback(1, () ->
-                        follower.setMaxPower(1)
-                )
-                .setLinearHeadingInterpolation(clearGateRisky.getHeading(), launchEdge.getHeading())
-                .build();
+
         GrabGate = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
-                                launchEdge,
-                                new Pose(108.2,56.7),
-                                grabGate
-                        )
+                                launch,
+                                new Pose(99.1, 47.5),
+                                grabGate)
                 )
                 .addParametricCallback(0.8, () ->
-                        follower.setMaxPower(0.7)
+                        follower.setMaxPower(0.5)
                 )
-                .addParametricCallback(0.99, () ->
+                .addParametricCallback(1, () ->
                         follower.setMaxPower(1)
                 )
-                .setLinearHeadingInterpolation(launchEdge.getHeading(),grabGate.getHeading())
+                .setLinearHeadingInterpolation(launch.getHeading(), grabGate.getHeading())
                 .build();
         LaunchGate = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
                                 grabGate,
-                                new Pose(105.8,66.9),
-                                launchEdge
+                                new Pose(96.6, 61.8),
+                                launch)
+                )
+                .setLinearHeadingInterpolation(grab2.getHeading(), launch.getHeading())
+                .build();
+
+        Launch1 = follower.pathBuilder()
+                .addPath(
+                        new BezierLine(grab1, launchFinal)
+                )
+                .setLinearHeadingInterpolation(grab1.getHeading(), launchFinal.getHeading())
+                .build();
+
+        Grab2 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                launchPre,
+                                new Pose(83.25, 63.62),
+                                new Pose(75.04, 51.58),
+                                grab2
                         )
                 )
                 .addParametricCallback(0.6, () ->
@@ -192,7 +171,40 @@ public class AutoRedCloseSolo extends CommandOpMode {
                 .addParametricCallback(1, () ->
                         follower.setMaxPower(1)
                 )
-                .setLinearHeadingInterpolation(grabGate.getHeading(), launchEdge.getHeading())
+                .setTangentHeadingInterpolation()
+                .build();
+
+        Launch2 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                grab2,
+                                new Pose(97.832, 54.873),
+                                launch)
+                )
+                .setLinearHeadingInterpolation(grab2.getHeading(), launch.getHeading())
+                .build();
+
+        Grab3 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                launch,
+                                new Pose(78, 61),
+                                new Pose(60, 29),
+                                grab3
+                        )
+                )
+                .setLinearHeadingInterpolation(launch.getHeading(), grab3.getHeading())
+                .build();
+        Launch3 = follower.pathBuilder()
+                .addPath(
+                        new BezierLine(grab3, launch3)
+                )
+                .setLinearHeadingInterpolation(grab3.getHeading(), launch3.getHeading())
+                .build();
+        Exit = follower.pathBuilder()
+                .addPath(
+                        new BezierLine(launch3, exit)
+                )
                 .build();
     }
 
@@ -212,8 +224,8 @@ public class AutoRedCloseSolo extends CommandOpMode {
         onofrei = new OnofreiSubsystem(hardwareMap);
         intake = new IntakeSubsystem(hardwareMap);
         intakeKicker = new IntakeKickerSubsystem(hardwareMap);
-        rampa = new RampaSubsystem(hardwareMap);
         tilt = new TiltSubsystem(hardwareMap);
+        rampa = new RampaSubsystem(hardwareMap);
         senzorTavan = new SenzorTavanSubsystem(hardwareMap);
         senzorRoata = new SenzorRoataSubsystem(hardwareMap);
         senzorGaura = new SenzorGauraSubsystem(hardwareMap);
@@ -234,54 +246,61 @@ public class AutoRedCloseSolo extends CommandOpMode {
                         new SequentialCommandGroup(
                                 new WaitCommand(500),
                                 new LaunchAll(robotStorage, telemetryM, follower, palete, onofrei, launcher, rampa, ALLIANCE)
-                        )
+                            )
                 ),
-
                 new ParallelCommandGroup(
-                        new Intake(robotStorage, telemetryM, intake, palete, senzorTavan, senzorRoata, senzorGaura, intakeKicker).withTimeout(5500),
+                        new Intake(robotStorage, telemetryM, intake, palete, senzorTavan, senzorRoata, senzorGaura, intakeKicker).withTimeout(6000),
                         new SequentialCommandGroup(
                                 new FollowPathCommand(follower, Grab2, true),
                                 new FollowPathCommand(follower, Launch2, true)
                         )
                 ),
-                new LaunchAll(robotStorage, telemetryM, follower, palete, onofrei, launcher, rampa, ALLIANCE),
-
                 new ParallelCommandGroup(
-                        new Intake(robotStorage, telemetryM, intake, palete, senzorTavan, senzorRoata, senzorGaura, intakeKicker).withTimeout(5500),
+                        new LaunchAll(robotStorage, telemetryM, follower, palete, onofrei, launcher, rampa, ALLIANCE),
+                        new SpitBalls(intake).withTimeout(1000)
+                ),
+                new ParallelCommandGroup(
+                        new Intake(robotStorage, telemetryM, intake, palete, senzorTavan, senzorRoata, senzorGaura, intakeKicker).withTimeout(6000),
                         new SequentialCommandGroup(
                                 new FollowPathCommand(follower, GrabGate, true),
                                 new InstantCommand(() -> {
-                                    follower.setMaxPower(1);
-                                }),
-                                new WaitCommand(600),
+                                    follower.setMaxPower(1);   }
+                                ),
+                                new WaitCommand(400),
                                 new FollowPathCommand(follower, LaunchGate, true)
                         )
                 ),
-                new LaunchAll(robotStorage, telemetryM, follower, palete, onofrei, launcher, rampa, ALLIANCE),
-
                 new ParallelCommandGroup(
-                        new Intake(robotStorage, telemetryM, intake, palete, senzorTavan, senzorRoata, senzorGaura, intakeKicker).withTimeout(5500),
-                        new SequentialCommandGroup(
-                                new FollowPathCommand(follower, GrabGate, true),
-                                new InstantCommand(() -> {
-                                    follower.setMaxPower(1);
-                                }),
-                                new WaitCommand(1000),
-                                new FollowPathCommand(follower, LaunchGate, true)
-                        )
+                        new LaunchAll(robotStorage, telemetryM, follower, palete, onofrei, launcher, rampa, ALLIANCE),
+                        new SpitBalls(intake).withTimeout(1000)
                 ),
-                new LaunchAll(robotStorage, telemetryM, follower, palete, onofrei, launcher, rampa, ALLIANCE),
-
-
+//                new ParallelCommandGroup(
+//                        new Intake(robotStorage, telemetryM, intake, palete, senzorTavan, senzorRoata, senzorGaura, intakeKicker).withTimeout(6000),
+//                        new SequentialCommandGroup(
+//                                new FollowPathCommand(follower, GrabGate, true),
+//                                new InstantCommand(() -> {
+//                                    follower.setMaxPower(1);   }
+//                                ),
+//                                new WaitCommand(300),
+//                                new FollowPathCommand(follower, LaunchGate, true)
+//                        )
+//                ),
+//                new ParallelCommandGroup(
+//                        new LaunchAll(robotStorage, telemetryM, follower, palete, onofrei, launcher, rampa, ALLIANCE),
+//                        new SpitBalls(intake).withTimeout(1000)
+//                ),
+                new TurnToCommand(follower, Math.toRadians(0)),
                 new ParallelCommandGroup(
-                        new Intake(robotStorage, telemetryM, intake, palete, senzorTavan, senzorRoata, senzorGaura, intakeKicker).withTimeout(4000),
+                        new Intake(robotStorage, telemetryM, intake, palete, senzorTavan, senzorRoata, senzorGaura, intakeKicker).withTimeout(6000),
                         new SequentialCommandGroup(
                                 new FollowPathCommand(follower, Grab1, true),
                                 new FollowPathCommand(follower, Launch1, true)
                         )
                 ),
-                new LaunchAll(robotStorage, telemetryM, follower, palete, onofrei, launcher, rampa, ALLIANCE),
-
+                new ParallelCommandGroup(
+                        new LaunchAll(robotStorage, telemetryM, follower, palete, onofrei, launcher, rampa, ALLIANCE),
+                        new SpitBalls(intake).withTimeout(1000)
+                ),
                 new InstantCommand(
                         () -> {
                             launcher.brake();
@@ -300,20 +319,9 @@ public class AutoRedCloseSolo extends CommandOpMode {
         telemetryM.addData("Loop Time", loopTime.milliseconds());
         telemetryM.addData("X", follower.getPose().getX());
         telemetryM.addData("Y", follower.getPose().getY());
-        telemetryM.addData("Heading", Math.toDegrees(follower.getPose().getHeading()));
-        telemetryM.addData("motif", robotStorage.getMotif()[0]);
+        telemetryM.addData("Heading", follower.getPose().getHeading());
         telemetryM.update(telemetry);
 
         loopTime.reset();
     }
-
-    @Override
-    public void end() {
-        robotStorage.updateAutoEndPose(follower.getPose());
-        super.end();
-    }
 }
-
-
-
-
